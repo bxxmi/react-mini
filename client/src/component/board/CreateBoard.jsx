b88474466da186ba0ea8ae736b4e06ef9cdf8a72
bxxmi <bmi6638@gmail.com>
2021-12-17 16:15:33 +0900

    feat: add axios logic

--- client/src/component/board/CreateBoard.jsx
+++ client/src/component/board/CreateBoard.jsx
@@ -1,26 +1,44 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import axios from "axios";
 
 const CreateBoard = () => {
-  const [board, setBoard] = useState({});
+  const [boardItem, setBoardItem] = useState({});
 
-  const onSubmit = () => {
-    console.log("submit");
+  const formRef = useRef();
+  const titleRef = useRef();
+  const contentRef = useRef();
+  const userRef = useRef();
+  const passwordRef = useRef();
+
+  const createBoard = (board) => {
+    console.log(board);
+    axios.post("/api/Board?type=save", { board }).then((response) => {
+      console.log(response);
+    });
   };
 
-  const onSubmitHandler = (e) => {
+  const onSubmit = (e) => {
     e.preventDefault();
-    console.log("submithandler");
+
+    const board = {
+      title: titleRef.current.value,
+      content: contentRef.current.value,
+      id: Date.now(),
+      insert_user: userRef.current.value,
+      view_count: 0,
+      write_password: passwordRef.current.value,
+    };
+
+    createBoard(board);
   };
 
   return (
-    <form onSubmit={onSubmit}>
-      <input type="text" name="title" placeholder="제목" />
-      <input type="text" name="content" placeholder="내용" />
-      <input type="password" placeholder="작성 비밀번호" />
-      <input type="text" placeholder="등록자" />
-      <input type="text" placeholder="등록일자" />
-      <button type="submit" onClick={onSubmitHandler}>
+    <form ref={formRef}>
+      <input ref={titleRef} type="text" name="title" placeholder="제목" />
+      <input ref={contentRef} type="text" name="content" placeholder="내용" />
+      <input ref={passwordRef} type="password" placeholder="작성 비밀번호" />
+      <input ref={userRef} type="text" placeholder="등록자" />
+      <button type="submit" onClick={onSubmit}>
         생성
       </button>
     </form>
