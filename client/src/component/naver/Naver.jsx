e2f17b5d87109941b4453b114953e8da7faed188
bxxmi <bmi6638@gmail.com>
2021-12-20 12:42:35 +0900

    feat: implement search product

--- client/src/component/naver/Naver.jsx
+++ client/src/component/naver/Naver.jsx
@@ -17,7 +17,6 @@ const Naver = () => {
       .post("/api/naverApi?type=search", { query: text })
       .then((response) => {
         const result = response.data.items[0];
-        console.log(result);
         setKeywordResult(result);
       });
   };
@@ -27,28 +26,52 @@ const Naver = () => {
   }, [keyword]);
 
   const onSearch = () => {
-    console.log("search!");
+    axios
+      .post("/api/naverApi?type=shopList", { query: keyword })
+      .then((response) => {
+        const result = response.data.items;
+        setProduct(result);
+      });
+
+    setKeywordResult([]);
+  };
+
+  const changeText = (e) => {
+    const text = e.target.innerText.replace("0", "");
+    setKeyword(text);
   };
 
   return (
-    <div>
-      <h2>최저가 상품 조회 및 등록 하기</h2>
-      <input
-        type="text"
-        name="search"
-        placeholder="search product"
-        onChange={inputKeyword}
-        onKeyPress={(e) => {
-          if (e.key === "Enter") onSearch();
-        }}
-      />
-      <ul>
-        {keywordResult &&
-          keywordResult.map((result) => {
-            return <li>{result}</li>;
+    <>
+      <div>
+        <h2>최저가 상품 조회 및 등록 하기</h2>
+        <input
+          type="text"
+          name="search"
+          placeholder="search product"
+          onChange={inputKeyword}
+          onKeyPress={(e) => {
+            if (e.key === "Enter") onSearch();
+          }}
+        />
+        <ul>
+          {keywordResult &&
+            keywordResult.map((result) => {
+              return <li onClick={changeText}>{result}</li>;
+            })}
+        </ul>
+      </div>
+      <div>
+        {product &&
+          Object.values(product).map((items) => {
+            return (
+              <p>
+                {items.title} {items.lprice} <button>등록</button>
+              </p>
+            );
           })}
-      </ul>
-    </div>
+      </div>
+    </>
   );
 };
 
