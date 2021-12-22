7153378f6e59927b4c6dc75300aba1864d4a3b47
bxxmi <bmi6638@gmail.com>
2021-12-22 21:07:07 +0900

    design: all desgin page design

--- client/src/component/board/BoardDetail.jsx
+++ client/src/component/board/BoardDetail.jsx
@@ -1,33 +1,53 @@
 import React, { useState } from "react";
 import EditBoard from "./EditBoard";
+import { Button } from "reactstrap";
+import styled from "styled-components";
 
 const BoardDetail = ({ location }) => {
   const [isOpen, setIsOpen] = useState(false);
 
-  const {
-    id,
-    title,
-    content,
-    insert_user,
-    insert_date,
-    view_count,
-    write_password,
-  } = location.state.item;
+  const { title, content, insert_user, view_count } = location.state.item;
 
   const onClick = () => {
     setIsOpen(!isOpen);
   };
 
   return (
-    <div>
-      {isOpen && <EditBoard item={location.state.item} />}
-      <h3>게시글 {title} 의 상세 정보</h3>
-      <p>
-        {title} / {content} / {insert_user}
-      </p>
-      <button onClick={onClick}>글 수정</button>
-    </div>
+    <>
+      <BoardDetailTemplate>
+        <BoardTitle>
+          <h3>{title}</h3>
+          <BoardSubTitle>
+            <h6>{insert_user}</h6>
+            <h6>조회수 {view_count}</h6>
+          </BoardSubTitle>
+        </BoardTitle>
+        <BoardContent>{content}</BoardContent>
+        <Button onClick={onClick}>글 수정</Button>
+      </BoardDetailTemplate>
+      {isOpen && <EditBoard item={location.state.item} isOpen={isOpen} />}
+    </>
   );
 };
+
+const BoardDetailTemplate = styled.div`
+  padding: 20px;
+`;
+
+const BoardTitle = styled.div`
+  border-bottom: 1px solid #ed214c;
+  margin-bottom: 20px;
+`;
+
+const BoardSubTitle = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const BoardContent = styled.div`
+  font-size: 18px;
+  height: 400px;
+  margin-bottom: 20px;
+`;
 
 export default BoardDetail;
--- client/src/component/board/EditBoard.jsx
+++ client/src/component/board/EditBoard.jsx
@@ -1,8 +1,19 @@
 import React, { useRef } from "react";
+import { useHistory } from "react-router-dom";
 import axios from "axios";
+import {
+  Button,
+  Form,
+  FormGroup,
+  Input,
+  Label,
+  Modal,
+  ModalBody,
+  ModalHeader,
+} from "reactstrap";
 
-const EditBoard = ({ item }) => {
-  const { id, title, content, insert_user, write_password } = item;
+const EditBoard = ({ item, isOpen }) => {
+  const { id, title, content, insert_user, write_password, view_count } = item;
 
   const formRef = useRef();
   const titleRef = useRef();
@@ -10,8 +21,24 @@ const EditBoard = ({ item }) => {
   const userRef = useRef();
   const passwordRef = useRef();
 
+  const history = useHistory();
+
   const onSubmit = (e) => {
     e.preventDefault();
+
+    if (!titleRef.current.value) {
+      alert("제목을 입력해주세요");
+      titleRef.current.focus();
+    } else if (!userRef.current.value) {
+      alert("작성자를 입력해주세요");
+      userRef.current.focus();
+    } else if (!contentRef.current.value) {
+      alert("내용을 입력해주세요");
+      contentRef.current.focus();
+    } else if (!passwordRef.current.value) {
+      alert("비밀번호를 입력해주세요");
+      passwordRef.current.focus();
+    }
 
     axios
       .post("/api/Board?type=save", {
@@ -19,12 +46,13 @@ const EditBoard = ({ item }) => {
         title: titleRef.current.value,
         content: contentRef.current.value,
         insert_user: userRef.current.value,
-        view_count: 0,
+        view_count,
         write_password: passwordRef.current.value,
       })
       .then((response) => {
         try {
-          console.log(response);
+          alert("게시글 수정이 완료됐습니다.");
+          history.goBack();
         } catch (e) {
           console.error("Error Encure: ", e);
         }
@@ -32,32 +60,54 @@ const EditBoard = ({ item }) => {
   };
 
   return (
-    <div>
-      <form ref={formRef}>
-        <input ref={titleRef} type="text" name="title" defaultValue={title} />
-        <input
-          ref={contentRef}
-          type="text"
-          name="content"
-          defaultValue={content}
-        />
-        <input
-          ref={userRef}
-          type="text"
-          name="user"
-          defaultValue={insert_user}
-        />
-        <input
-          ref={passwordRef}
-          type="text"
-          name="password"
-          defaultValue={write_password}
-        />
-        <button type="submit" onClick={onSubmit}>
-          수정
-        </button>
-      </form>
-    </div>
+    <Modal centered fullscreen="md" size="lg" isOpen={isOpen}>
+      <ModalHeader>게시글 수정</ModalHeader>
+      <ModalBody>
+        <Form ref={formRef}>
+          <FormGroup>
+            <Label>제목</Label>
+            <Input
+              innerRef={titleRef}
+              type="text"
+              name="title"
+              defaultValue={title}
+              maxLength="50"
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label>작성자</Label>
+            <Input
+              innerRef={userRef}
+              type="text"
+              name="writer"
+              defaultValue={insert_user}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label>내용</Label>
+            <Input
+              innerRef={contentRef}
+              type="textarea"
+              name="content"
+              defaultValue={content}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label>게시글 비밀번호</Label>
+            <Input
+              innerRef={passwordRef}
+              type="password"
+              name="password"
+              defaultValue={write_password}
+              maxLength="10"
+            />
+          </FormGroup>
+          <Button type="submit" onClick={onSubmit}>
+            수정
+          </Button>
+        </Form>
+      </ModalBody>
+    </Modal>
   );
 };
 
